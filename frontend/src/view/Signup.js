13c53b707e000dce513d1b1843a6fb98231c7f2a
bgKMcuy <karyadi_cm13@apps.ipb.ac.id>
2021-12-01 08:24:41 +0700

    add ui

--- frontend/src/view/Signup.js
+++ frontend/src/view/Signup.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, {useState, useContext} from "react";
+import clsx from 'clsx';
+import {Button, FormControl, Grid, makeStyles, Paper, TextField} from '@material-ui/core';
+import { Box } from "@mui/system";
 
-const Signup = () =>{
+const useStyles = makeStyles((theme) => ({
+    root:{
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    margin:{
+        margin: theme.spacing(1),
+    },
+    textField:{
+        marginTop: 20,
+        marginBottom: 20,
+        display: 'block',
+    },
+    paperBox:{
+        elevation: 10,
+        padding: 20,
+        display: 'flex',
+        flexDirection:"column",
+        flexWrap: 'wrap',
+        height:'370px', 
+        width:'700px',
+        margin:'100px auto',
+        backgroundColor: '#F2F2F2',
+    },
+}));
+
+const Signup=() =>{
+    const style = useStyles();
+    const [error, setError] = useState("invalid password");
+    
+   
     return(
-        <div>Signup</div>
+   
+        <Paper className={style.paperBox}>
+            <h2 align='center'>Sign Up as Influencer</h2>
+            <form noValidate autoComplete='off'>
+                <TextField  
+                    className={style.textField}
+                    variant='outlined'
+                    placeholder='Masukkan Username'
+                    fullWidth />
+                <TextField  
+                    className={style.textField}
+                    variant='outlined'
+                    type='password' 
+                    placeholder='Password'
+                    fullWidth/>
+                <Button
+                    type='submit'
+                    color='secondary'
+                    variant='contained'>
+                        SignUp
+                </Button>
+            </form>
+            {error && <p className='error'>{error}</p>}
+            
+        </Paper>
     )
-}
 
+}
 export default Signup;
--- frontend/src/view/Utama.js
+++ frontend/src/view/Utama.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Utama = props =>{
     return(
-        <div>Homepage</div>
-    )
+        <>
+        <div className="flex justify-center">
+            <img className = 'img order-2 mt-5' src="Frame 2.png" width="500" height="600"></img>
+            <div className="order-1 flex flex-col p-16">
+                <div className="text-5xl font-semibold mt-16 ml-16">Promosikan produk dan dapatkan fee-nya!</div>
+                <div className="sm extralight text-left	text-gray-500 mt-20 ml-16">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+the industry's standard dummy text ever since the 1500s,</div>
+                <div className="grid-col-2 mx-32 my-10"> 
+                    <button class="bg-purple-800 shadow-xl	rounded-lg">SAYA PEMILIK BRAND</button>
+                    <button class="bg-purple-800 shadow-xl	rounded-lg ml-16">SAYA INFLUENCER</button>
+                </div>
+            </div>
+        </div>
+        
+        
+    
+        </>
+    );
 }
 
 export default Utama;
